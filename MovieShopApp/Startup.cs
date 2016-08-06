814a715232e4028a896007f4f887e92034119a67
MilanToncic <milantoncic93@gmail.com>
2016-08-06 19:01:56 +0200

    Add project files.

---
+++ MovieShopApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieShopApp.Startup))]
+namespace MovieShopApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
